f52c3394ab39ee0ad74fd3d4abd46bf0ac3093fd
TATA <bereketsewnet306@gmail.com>
2024-02-10 15:21:53 -0800

    commit on Sat, Feb 10 6:21 PM this is last commite on windows os android studio i change to kali linux of better performance

--- lib/pages/student_page/book_collections.dart
+++ lib/pages/student_page/book_collections.dart
@@ -3,9 +3,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BooksCollections extends StatefulWidget {
-   BooksCollections({Key? key, this.bookName, required this.bookUrl}) : super(key: key);
-   String? bookName;
-   String bookUrl;
+  BooksCollections({Key? key, this.bookName, required this.bookUrl})
+      : super(key: key);
+  String? bookName;
+  String bookUrl;
 
   @override
   State<BooksCollections> createState() => _BooksCollectionsState();
@@ -13,17 +14,21 @@ class BooksCollections extends StatefulWidget {
 
 class _BooksCollectionsState extends State<BooksCollections> {
   PDFDocument? document;
-  void downloadPDF() async{
+
+  void downloadPDF() async {
     document = await PDFDocument.fromURL(widget.bookUrl);
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: document != null ? PDFViewer(
-        document: document!,
-      ) : Center(child,),
+      body: document != null
+          ? PDFViewer(
+              document: document!,
+            )
+          : const Center(
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
